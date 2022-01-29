9d7b819cc04956fdac28569402ed6bb559ee2427
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-30 05:09:01 +0800

    fix: hant title

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { t } from '~/i18n'
+useTitle(computed(() => `${t('name')} - ${t('description')}`))
+</script>
+
 <template>
   <main font-sans text="center gray-700 dark:gray-200" select-none>
     <Navbar />
