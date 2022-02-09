0187f06ca3b886c793f1f7391be97db13b89161f
TonyCrane <tonycrane@foxmail.com>
2022-02-10 00:12:10 +0800

    fix: wrong initials order

--- src/logic/lang/pinyin.ts
+++ src/logic/lang/pinyin.ts
@@ -1,4 +1,4 @@
-export const pinyinInitials = 'b p m f d t n l g k h j q r x w y z c s zh ch sh'.split(/\s/g)
+export const pinyinInitials = 'b p m f d t n l g k h j q r x w y zh ch sh z c s'.split(/\s/g)
 
 export const pinyinFinals = 'a o e ai ei ao ou an en ang eng ong er i ia io ie iao iu ian in iang ing iong u ua uo uai ui uan un uang ueng v ve van vn'
   .split(/\s/g)
