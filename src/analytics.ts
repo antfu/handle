74a54346a2315084e278bf1650579bef7063e4da
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-19 18:54:57 +0800

    chore: supabase init

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,20 +1,46 @@
 import { nanoid } from 'nanoid'
+import { createClient } from '@supabase/supabase-js'
 import { dayNo } from './state'
 import { history, inputMode } from './storage'
+import type { TriesMeta } from './logic'
+
+const SUPABASE_URL = 'https://lxmqadkqgpcewmfffyig.supabase.co'
+const SUPABASE_PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4bXFhZGtxZ3BjZXdtZmZmeWlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUyNjI3NzUsImV4cCI6MTk2MDgzODc3NX0.2b1IvYNjFBCQ5wsHbkt1xBoupaaXgWL9VgVfHCQtcNQ'
+
+// Lazy initialize the client
+let _supabase: ReturnType<typeof createClient> | undefined
 
 // TODO:
-let _uid = localStorage.getItem('uid')
+let _uid = localStorage.getItem('handle-uid')!
 if (!_uid) {
   _uid = nanoid()
-  localStorage.setItem('uid', _uid)
+  localStorage.setItem('handle-uid', _uid)
 }
-let timezone = ''
-try {
-  timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+export async function getSupabase() {
+  if (!_supabase)
+    _supabase = _supabase || createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY)
+  return _supabase
 }
-catch {}
 
 export const analyticSetting = useStorage<number>('handle-accept-analytics', 0) // 0: not sure, 1: ok, -1: no
+
+export function preparePayload(day: number, meta: TriesMeta) {
+  if (!meta || (!meta.passed && !meta.failed) || meta.sent)
+    return
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { sent, ...rest } = toRaw(meta)
+  const payload = {
+    id: `${_uid}-${day}`,
+    uid: _uid,
+    day,
+    inputMode: inputMode.value,
+    ...rest,
+  }
+
+  return payload
+}
 
 export async function sendAnalytics(day = dayNo.value) {
   if (analyticSetting.value < 0)
@@ -22,26 +48,34 @@ export async function sendAnalytics(day = dayNo.value) {
 
   const meta = history.value[day]
 
-  if (!meta || (!meta.passed && !meta.failed) || meta.sent)
+  uploadPayloads([preparePayload(day, meta)])
+}
+
+export async function uploadPayloads(payloads: ReturnType<typeof preparePayload>[]) {
+  const items = payloads.filter(<T>(i: T | undefined): i is T => !!i)
+  if (!items.length)
     return
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { start, end, ...rest } = toRaw(meta)
-  const payload = {
-    uid: _uid,
-    day,
-    inputMode: inputMode.value,
-    ...rest,
-    ts: Date.now(),
-    timezone,
-  }
+  const s = await getSupabase()
+  const { data, error } = await s.from('uploads').insert(items)
 
-  // TODO:
-  // meta.sent = true
-  console.log({ payload })
+  // mark as sent
+  // if (!error) {
+  //   items.forEach((i) => {
+  //     if (history.value[i.day])
+  //       history.value[i.day].sent = true
+  //   })
+  // }
+
+  console.log({ data, error })
 }
 
 export async function sendHistoryAnalytics() {
-  for (const day of Object.keys(history.value))
-    await sendAnalytics(+day)
+  if (analyticSetting.value < 0)
+    return
+
+  const payloads = Object.entries(history.value)
+    .map(([day, meta]) => preparePayload(Number(day), meta))
+
+  await uploadPayloads(payloads)
 }
