788a72007e29ec5f017afc25beaef204ec7b752e
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-19 19:44:17 +0800

    chore: setup netlify functions

---
+++ netlify/functions/hello.ts
@@ -0,0 +1,8 @@
+import type { Handler } from '@netlify/functions'
+
+export const handler: Handler = async(event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
+  }
+}
