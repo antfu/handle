f2b27f50a07d90ec0701439a32304ecd0baa5701
yuiffy <yuiffy@qq.com>
2022-02-22 22:20:21 +0800

    fix: 统一为北京时间0点更新题目，防止各地更新时间不统一

--- src/logic/constants.ts
+++ src/logic/constants.ts
@@ -1,5 +1,8 @@
 export const WORD_LENGTH = 4
 export const TRIES_LIMIT = 10
-export const START_DATE = new Date(2022, 0, 0)
+const START_DATE = new Date(2022, 0, 0)
+START_DATE.setUTCFullYear(2022, 0, 0)
+START_DATE.setUTCHours(-8, 0, 0, 0)
+export { START_DATE }
 export const RANDOM_SEED = 'handle'
 export const DAYS_PLAY_BACK = 3
