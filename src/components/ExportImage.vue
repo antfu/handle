3e1f14b70dfd769c99cf336efef48dea070b5588
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-28 21:24:53 +0800

    chore: update

--- src/components/ExportImage.vue
+++ src/components/ExportImage.vue
@@ -43,7 +43,7 @@ async function download() {
   <div v-if="isMobile" op50 mb4>
     {{ t('press-and-download-image') }}
   </div>
-  <img v-if="dataUrl" :src="dataUrl" w-80 min-h-10 border="~ base rounded" shadow>
+  <img v-if="dataUrl" :src="dataUrl" w-80 min-h-10 border="~ base rounded">
   <div v-else w-80 border="~ base rounded" p4 animate-pulse>
     {{ t('rendering') }}
   </div>
