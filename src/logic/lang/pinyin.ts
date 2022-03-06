7f457568627c79b356e57e660b0800fc5e1c6e9a
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-07 01:46:49 +0800

    chore: cleanup

--- src/logic/lang/pinyin.ts
+++ src/logic/lang/pinyin.ts
@@ -1,10 +1,9 @@
 export const pinyinInitials = 'b p m f d t n l g k h j q r x w y zh ch sh z c s'.split(/\s/g)
 
-export const pinyinFinals = 'a o e ai ei ao ou an en ang eng ong er i ia io ie iao iu ian in iang ing iong u ua uo ue uai ui uan un uang ueng v ve van vn'
+export const pinyinFinals = 'a o e ai ei ao ou an en ang eng ong er i ia io ie iao iu ian in iang ing iong u ua uo ue uai ui uan un uang v ve van vn'
   .split(/\s/g)
   .sort((a, b) => {
     const i = a.length - b.length
-    if (i === 0)
-      return a.localeCompare(b)
+    return a.localeCompare(b)
     return i
   })
