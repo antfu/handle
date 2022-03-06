f6ba6882d90f2c9a5535251e590b9b3c99c87ce6
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-06 19:07:43 +0800

    chore: add ue to cheatsheet

--- src/logic/lang/pinyin.ts
+++ src/logic/lang/pinyin.ts
@@ -1,6 +1,6 @@
 export const pinyinInitials = 'b p m f d t n l g k h j q r x w y zh ch sh z c s'.split(/\s/g)
 
-export const pinyinFinals = 'a o e ai ei ao ou an en ang eng ong er i ia io ie iao iu ian in iang ing iong u ua uo uai ui uan un uang ueng v ve van vn'
+export const pinyinFinals = 'a o e ai ei ao ou an en ang eng ong er i ia io ie iao iu ian in iang ing iong u ua uo ue uai ui uan un uang ueng v ve van vn'
   .split(/\s/g)
   .sort((a, b) => {
     const i = a.length - b.length
