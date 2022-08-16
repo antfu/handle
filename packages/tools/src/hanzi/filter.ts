2b8c85d1c74cd3549b3548fd8654a2a7052eebbc
Yang Mingshan <y.mingshan3@gmail.com>
2022-08-16 12:26:54 +0800

    fix: hanzi regexp

--- packages/tools/src/hanzi/filter.ts
+++ packages/tools/src/hanzi/filter.ts
@@ -1,6 +1,6 @@
 export function filterNonChineseChars(input: string) {
   return Array.from(input)
-    .filter(i => /\p{Script=Han}/u.test(i))
+    .filter(i => /\p{Unified_Ideograph}/u.test(i))
     .join('')
 }
 
