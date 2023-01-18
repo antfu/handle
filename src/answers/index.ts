78ea2f99959551d9370e6a415701ae39f7ed23d7
Anthony Fu <anthonyfu117@hotmail.com>
2023-01-18 17:10:37 +0100

    chore: lint

--- src/answers/index.ts
+++ src/answers/index.ts
@@ -6,7 +6,7 @@ export function getAnswerOfDay(day: number) {
   let answer: string[]
   // When the day is out of range, pick a random answer from the list.
   if (day > answers.length) {
-    const seed = seedrandom('day-' + day)()
+    const seed = seedrandom(`day-${day}`)()
     answer = answers[Math.floor(seed * answers.length)]
   }
   else {
